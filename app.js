afef9d943f60c2336bb5544a70655f49aca7c10d
minghung1191 <minghung1191@gmail.com>
2024-06-25 23:30:31 +0800

    feat: add search funtion to listing

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ const express = require('express')
 const { engine } = require('express-handlebars')
 const app = express()
 const port = 3000
-const restaurants = require('./public/jsons/restaurants.json').results
-const BASE_IMG_URL = 'https://assets-lighthouse.s3.amazonaws.com/uploads/image/file/'
+const restaurants = require('./public/jsons/restaurant.json').results
 
 app.engine('.hbs', engine({extname: '.hbs'}))
 app.set('view engine', '.hbs')
@@ -15,14 +14,23 @@ app.get('/', (req, res) => {
 })
 
 app.get('/restaurants', (req, res) => {
-    res.render('index', { restaurants, BASE_IMG_URL })  
+    const keyword = req.query.keyword?.trim()
+    const matchedRestaurants = keyword ? restaurants.filter( (rt) =>
+        Object.values(rt).some((property) => {
+          if (typeof property === 'string') {
+            return property.toLowerCase().includes(keyword.toLowerCase())
+          }
+          return false
+        })
+      ) : restaurants
+    res.render('index', { restaurants: matchedRestaurants, keyword })
 })
 
 app.get('/restaurants/:id', (req, res) => {
     const id = req.params.id
     const restaurant = restaurants.find((rt) => rt.id.toString() === id)
-    res.render('show', { restaurant, BASE_IMG_URL })
-  })
+    res.render('show', {restaurant})
+})
 
 app.listen(port, () => {
   console.log(`express server is running on http://localhost:${port}`)
